fa9fe6eb6ca6c932b254ed197b72b496590da5be
Peter Naydenov <peter.naydenov@gmail.com>
2026-07-21 20:43:10 +0300

    types: update types for v.2.5.0

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,12 +1,12 @@
-export default notice;
 declare function notice(): {
     on: (e: string | Symbol, fn: Function) => void;
     once: (e: string | Symbol, fn: Function) => void;
-    off: (e: string | Symbol, fx?: Function) => void;
+    off: (e: string | Symbol, fx?: Function | undefined) => void;
     reset: () => void;
-    emit: (...args: any[]) => void;
+    emit: (e: string | Symbol, ...args?: any[]) => void;
     stop: (e: string | Symbol) => void;
     start: (e: string | Symbol) => void;
     debug: (val: boolean, header?: string) => void;
 };
+export default notice;
 //# sourceMappingURL=main.d.ts.map
